1e4d19e6f91597c766fa72f00a2ef9c49131d220
iamzz <iamzz@iamzz.cn>
2024-12-25 17:02:44 +0800

    兼容Laravel最新版的 修改laravel最低版本为11 修改php最低版本为8.2 删除doctrine/dbal依赖,使用laravel/schema代替

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -4,16 +4,16 @@
     <section class="sidebar">
 
         <!-- Sidebar user panel (optional) -->
-        <div class="user-panel">
-            <div class="pull-left image">
-                <img src="{{ Admin::user()->avatar }}" class="img-circle" alt="User Image">
-            </div>
-            <div class="pull-left info">
-                <p>{{ Admin::user()->name }}</p>
-                <!-- Status -->
-                <a href="#"><i class="fa fa-circle text-success"></i> {{ trans('admin.online') }}</a>
-            </div>
-        </div>
+{{--        <div class="user-panel">--}}
+{{--            <div class="pull-left image">--}}
+{{--                <img src="{{ Admin::user()->avatar }}" class="img-circle" alt="User Image">--}}
+{{--            </div>--}}
+{{--            <div class="pull-left info">--}}
+{{--                <p>{{ Admin::user()->name }}</p>--}}
+{{--                <!-- Status -->--}}
+{{--                <a href="#"><i class="fa fa-circle text-success"></i> {{ trans('admin.online') }}</a>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
         @if(config('admin.enable_menu_search'))
         <!-- search form (Optional) -->
